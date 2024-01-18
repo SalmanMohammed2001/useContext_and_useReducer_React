7f7e1570970252a18981a72ad9f47376fe6c8769
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-19 01:03:40 +0530

    login form daata initialize 3

--- src/util/authValidate.js
+++ src/util/authValidate.js
@@ -1,8 +1,8 @@
 export const submitHandle=(username,password,setFun)=>{
     setFun(pre=>({...pre,
-        userId:usernameRef.current.value,
-        name:usernameRef.current.value,
-        password:passwordRef.current.value
+        userId:username,
+        name:password,
+        password:username
 
     }))
 }
