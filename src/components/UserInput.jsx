a5463952b2bc49c0c354cb020f0e356294fe885a
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-19 02:17:17 +0530

    ok

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,21 +1,59 @@
 import React, {useContext, useRef} from 'react';
 import {userActivity} from "../context/UserActivity.jsx";
 import {userData} from "../context/UserAuth.jsx";
+import {submitUserActivity} from "../util/authValidate.js";
 
-const UserInput=()=> {
+const UserInput = () => {
 
-    const { image, name, city, position, setImage, setName, setCity, setPosition}=useContext(userActivity)
+    const {image, name, city, position, setImage, setName, setCity, setPosition} = useContext(userActivity)
 
-    const {userDetails}=useContext(userData)
+    const {userDetails} = useContext(userData)
 
-        const imageReg=useRef()
-        const nameReg=useRef()
-        const cityReg=useRef()
-        const positionReg=useRef()
+    const imageReg = useRef()
+    const nameReg = useRef()
+    const cityReg = useRef()
+    const positionReg = useRef()
 
 
     return (
-        <div>UserInput</div>
+        <div>
+            <br/>
+            <br/>
+
+            {userDetails.userId ? <div>
+                <span>Image: </span>
+                <input ref={imageReg} type="text" placeholder={"image"}/> <br/><br/>
+
+                <span>name: </span>
+                <input ref={nameReg} type="text" placeholder={"name"}/> <br/><br/>
+
+                <span>city: </span>
+                <input ref={cityReg} type="text" placeholder={"city"}/> <br/><br/>
+
+                <span>position: </span>
+                <input ref={positionReg} type="text" placeholder={"position"}/> <br/><br/>
+
+                <br/>
+                <button onClick={() => submitUserActivity(
+                    imageReg,
+                    nameReg,
+                    cityReg,
+                    positionReg,
+                    setImage,
+                    setName,
+                    setCity,
+                    setPosition
+                )}> submit
+                </button>
+                <br/>
+                <br/>
+                <p> {image}</p>
+                <p>   {name}</p>
+                <p>  {city}</p>
+                <p>  {position}</p>
+
+            </div> : ''}
+        </div>
     );
 }
 
--- src/util/authValidate.js
+++ src/util/authValidate.js
@@ -6,3 +6,11 @@ export const submitHandle=(username,password,setFun)=>{
 
     }))
 }
+
+export  const  submitUserActivity=(image,name,city,position,setFunImage,setFunName,setFunCity,setFunPosition)=>{
+    setFunImage(image.current.value)
+    setFunName(name.current.value)
+    setFunCity(city.current.value)
+    setFunPosition(position.current.value)
+
+}
