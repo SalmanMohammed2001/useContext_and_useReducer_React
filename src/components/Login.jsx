2803b5f7eef15e70b26fddd8d44a2dbc3068ebd5
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-01-19 01:00:35 +0530

    login form daata initialize

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {userData} from "../context/UserAuth";
+import {useRef} from "react";
 
 const Login=()=> {
+    const {userDetails,setUserDetails}= useContext(userData)
+    const usernameRef=useRef()
+    const passwordRef=useRef()
+
+
+    const submitHandle=()=>{
+       setUserDetails(pre=>({...pre,
+           userId:usernameRef.current.value,
+           name:usernameRef.current.value,
+           password:passwordRef.current.value
+
+       }))
+    }
     return (
         <div>
-          <span> User Name : </span>
-            <input type="text" placeholder={"user name...."}/> <br/> <br/>
-            <span> Password : </span>
-            <input type="password" placeholder={"password...."}/>
+            {userDetails.userId?<p>your are logged in your computer :{userDetails.name}</p>:<>
+                <span> User Name : </span>
+                <input ref={usernameRef} type="text" placeholder={"user name...."}/> <br/> <br/>
+                <span> Password : </span>
+                <input ref={passwordRef} type="password" placeholder={"password...."}/> <br/> <br/>
+                <button onClick={submitHandle}> submit </button>
+
+            </>}
         </div>
     );
 }
